cf9b6e2054b033815ed2dab2a44236e677c5024e
Kavian Darvish <kavian1991@gmail.com>
2020-01-16 18:44:37 -0500

    created the initial round of user authentiction, routing, and database structure

---
+++ controller/controller.js
@@ -0,0 +1,133 @@
+const query = require("../models/model.js").module.query;
+const uuidv1 = require("uuid/v1");
+const bcrypt = require("bcrypt");
+
+const dataCleaner = async (...data) => {
+	let newData =  data.map(  (a) => { 
+		return a.replace(/ /g,"");
+	});
+	return newData;
+};
+
+const cleanUserData = (submitedData) => {
+	let {
+		userFirstName,
+		userLastName,
+		userEmail,
+		userPhone,
+		userCompany,
+		userRole,
+
+		userPortfolio,
+		userLinkedIn,
+		userInstagram,
+		userStyle
+	} = submitedData;
+
+	if (
+		userFirstName.replace(/\s/g, "") === "" ||
+    userLastName.replace(/\s/g, "") === "" ||
+    userEmail.replace(/\s/g, "") === "" ||
+    userPhone.replace(/\s/g, "") === "" ||
+    userCompany.replace(/\s/g, "") === "" || 
+    userRole.replace(/\s/g, "") === ""
+	)
+	{
+		return false;
+	} else {
+		// userFirstName=userFirstName.replace(/\s/g, ''),
+		// userLastName=userLastName.replace(/\s/g, ''),
+		// userEmail=userEmail.replace(/\s/g, ''),
+		// userPhone=userPhone.replace(/\s/g, ''),
+		// userCompany=userCompany.replace(/\s/g, ''),
+		// userRole=userRole.replace(/\s/g, ''),
+  
+		// userPortfolio=userPortfolio.replace(/\s/g, ''),
+		// userLinkedIn=userLinkedIn.replace(/\s/g, ''),
+		// userInstagram=userInstagram.replace(/\s/g, ''),
+		// userStyle=userStyle.replace(/\s/g, '')
+
+		return {
+			userFirstName,
+			userLastName,
+			userEmail,
+			userPhone,
+			userCompany,
+			userRole,
+			userPortfolio,
+			userLinkedIn,
+			userInstagram,
+			userStyle
+		};
+	}
+};
+
+const userExists = async (user) => {
+	let result = await query.getUser(user);
+	if (result === false) {
+		return false;
+	} else {
+		return result;
+	}
+};
+
+const handleSignIn = async (req, res, result , user , pass) => {
+	if ((bcrypt.compareSync(pass, result[0]["hashCode"])) === true){
+		req.session.userId = result[0]["unique_id"];
+		// console.log(result[0]["unique_id"],'!?')
+		res.send({goTo: `userProfile/${result[0]["unique_id"]}`});
+		return;
+	}
+};
+
+const handleSignUp = async (user , pass) => {
+	const hash = await hasher(pass);
+	const result = await query.insert( uuidv1() , user, hash);
+	return result[0];  
+};
+
+const getUserProfile = async (userId) => {
+	const result = await query.getProfile(userId);
+	return result[0];
+};
+
+
+const hasher = (myPlaintextPassword) => {
+	const saltRounds = 10;
+	const salt = bcrypt.genSaltSync(saltRounds);
+	const hash = bcrypt.hashSync(myPlaintextPassword, salt);
+	return hash;
+};
+
+
+const submitTheData = async (submitedData , uniq_id ) => {
+	const result = await query.checkUserProfileExists(uniq_id);
+	if ( !result.length ){
+		// doesnt exists, must create
+		const response = query.createNewUserProfile(submitedData , uniq_id);
+		if (response === false){
+			//send error msg
+		} else {
+			//refresh page
+		}
+	} else {
+		//does exist, must update
+		const res = query.updateExistingUserProfile(submitedData , uniq_id);
+		if (res === false){
+			//send error msg
+		} else {
+			//refresh page
+		}
+	}
+  
+};
+
+module.exports = {
+	userExists,
+	handleSignIn,
+	handleSignUp,
+	dataCleaner,
+	cleanUserData,
+	getUserProfile,
+	submitTheData
+};
--- server.js
+++ server.js
@@ -1,70 +1,135 @@
-// Dependencies
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const path = require("path");
-// const mysql = require("mysql");
 
-// Module Dependencies
-// const db_config = require("./config/connection");
+const controllers = require("./controller/controller.js");
 
-// Create an instance of the express app.
-var app = express();
+const app = express();
 
-// Added so body parser can handle post requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Host Static Files so css and js files can be retrieved
-app.use(express.static(path.join(__dirname, "/public")));
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 9090;
-
-// Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routes
+const TIME = 1000 * 60 * 15;
+const {
+	PORT = 9090,
 
-// Use Handlebars to render the main index.html page with the plans in it.
-app.get("/", function(req, res) {
-	res.render("index", 
-		{});
+	//cookie related
+	SESS_NAME = "sid",
+	SESS_SECRET = "thisIsTheSecretKey",
+	SESS_LIFETIME = TIME //set to 15 minutes
+} = process.env;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(session({
+	name: SESS_NAME,
+	resave: false, //dont store if cookie was not modified
+	saveUninitialized: false, //dont store empty cookies
+	secret: SESS_SECRET, // the key to sign the cookie
+	cookie:{
+		maxAge: SESS_LIFETIME, //life-time of cookie is 15 minutes
+		sameSite: true, //only accepts cookies from the same domain
+	}
+}));
+
+
+
+//this function will redirect the users to the login if they are not authenticated
+const redirectLogin = (req,res,next) => {
+	if (!req.session.userId) {
+		res.redirect("/");
+	} else {
+		next();
+	}
+};
+
+app.get("/" , ( req, res )=>{
+	res.render("index");
 });
 
-app.get("/userProfile", function(req, res) {
-	res.render("userProfile", 
-		{});
+
+app.post("/signIn" , async (req,res)=>{
+	const userData = await controllers.dataCleaner(req.body.user, req.body.pass);
+	const user = userData[0]; const pass = userData[1];
+	const result = await controllers.userExists(user);
+	if ( result === false ){
+		return res.status(404).send();
+	} else {
+		await controllers.handleSignIn(req, res, result , user , pass);
+	}
 });
 
-// will be as follows /QaRd/:username
-app.get("/userResume", function(req, res) {
-	res.render("userResume", 
-		{
-			userFirstName: "Andy",
-			userLastName: "Durette",
-			userEmail: "duretteandy@gmail.com",
-			userPhone: "6476077597",
-			userCompany: "U of T Student",
-			userRole: "Full Stack Developer",
-			userPortfolio: "andydurette.com",
-			userLinkedIn: "https://www.linkedin.com/in/andy-durette/",
-			userInstagram: "https://www.instagram.com/andy_cooks/?hl=en",
-			userStyle: "carbon"
-		});
+
+app.post("/signUp" , async (req,res) => {
+	const userData = await controllers.dataCleaner(req.body.user, req.body.pass);
+	const user = userData[0]; const pass = userData[1];
+	let result = await controllers.userExists(user,pass,"signUp");
+	if ( result === false) {
+		await controllers.handleSignUp(user, pass);
+		return res.status(200).send();
+	} else {
+		return res.status(400).send();
+	}
 });
 
-/*  POST REQUEST */
-app.post("/api/updateUser", (req, res) => {
-	console.log(req.body);
-	res.send("200");
+app.get("/logout" , async (req, res) => {
+	req.session.destroy(err=>{
+		if (err) {
+			return res.redirect("/signIn");
+		}
+
+		res.clearCookie(SESS_NAME);
+		res.redirect("/signIn");
+	});
 });
 
-// End of request Handling
+app.get("/userProfile/:user" , redirectLogin , async (req,res)=>{
+	const {user} = req.params;
+	const data = await controllers.getUserProfile(user);
+	res.render("userProfile",{
+		username: data.userName
+	});
+});
 
-// Start our server so that it can begin listening to client requests.
+app.post("/api/updateUser" ,redirectLogin, async (req,res)=>{
+	let submitedData = req.body;
+	const uniq_id = req.session.userId;
+	submitedData = controllers.cleanUserData(submitedData);
+	if (submitedData === false) {
+		res.status(400).send();
+	} else {
+		controllers.submitTheData(submitedData , uniq_id );
+	}
+});
+
+// // will be as follows /QaRd/:username
+// app.get("/userResume", function(req, res) {
+// 	res.render("userResume", 
+// 		{
+// 			userFirstName: "Andy",
+// 			userLastName: "Durette",
+// 			userEmail: "duretteandy@gmail.com",
+// 			userPhone: "6476077597",
+// 			userCompany: "U of T Student",
+// 			userRole: "Full Stack Developer",
+// 			userPortfolio: "andydurette.com",
+// 			userLinkedIn: "https://www.linkedin.com/in/andy-durette/",
+// 			userInstagram: "https://www.instagram.com/andy_cooks/?hl=en",
+// 			userStyle: "carbon"
+// 		});
+// });
+
+// /*  POST REQUEST */
+// app.post("/api/updateUser", redirectLogin, (req, res) => {
+// 	console.log(req.body);
+// 	res.send("200");
+// });
+
+
+
 app.listen(PORT, function() {
 	// Log (server-side) when our server has started
 	console.log("Server listening on: http://localhost:" + PORT);
