baec100f26955c096873127d7334594f24c7a5e8
Andy Durette <duretteandy@gmail.com>
2020-01-18 19:31:38 -0500

    Updated index page and fixed mobile view

--- public/assets/js/yearScript.js
+++ public/assets/js/yearScript.js
@@ -1,6 +1,6 @@
 // Sets year in footer copyright
 document.addEventListener("DOMContentLoaded", function(){
-	if (typeof(document.getElementById("year")) != 'undefined' && document.getElementById("year") != null){
-	document.getElementById("year").innerHTML = new Date().getFullYear();
+	if (typeof(document.getElementById("year")) != "undefined" && document.getElementById("year") != null){
+		document.getElementById("year").innerHTML = new Date().getFullYear();
 	}
 });
